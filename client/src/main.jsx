0a52ecc3277a7014426eaf4c4e912c1f02632d56
RXGabriel <gabriel2005rf@gmail.com>
2024-09-03 17:09:07 -0300

    feat: add Toaster component to main entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { Toaster } from "./components/ui/toaster.jsx";
 import { Provider } from "react-redux";
+import App from "./App.jsx";
 import "./index.css";
 import store from "./store/store.js";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </BrowserRouter>
 );
