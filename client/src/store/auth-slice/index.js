1a953f596c7af9d929190ed82991d32f502d6ff5
RXGabriel <gabriel2005rf@gmail.com>
2024-09-06 10:38:16 -0300

    feat: add checkAuth action

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -36,6 +36,20 @@ const logoutUser = createAsyncThunk("/auth/logout", async () => {
   );
   return response.data;
 });
+const checkAuth = createAsyncThunk("/auth/check-auth", async () => {
+  const response = await axios.get(
+    "http://localhost:5000/api/auth/check-auth",
+    {
+      withCredentials: true,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+      },
+    }
+  );
+
+  return response.data;
+});
 
 const authSlice = createSlice({
   name: "auth",
@@ -65,6 +79,19 @@ const authSlice = createSlice({
         state.isAuthenticated = action.payload.success;
       })
       .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
