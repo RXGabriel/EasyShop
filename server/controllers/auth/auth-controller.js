e9344b29cfa252b11222df7daef8253d3da3b19e
RXGabriel <gabriel2005rf@gmail.com>
2024-09-06 11:01:22 -0300

    fix: correct missing check for User existence

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -32,7 +32,7 @@ const loginUser = async (req, res) => {
 
   try {
     const checkUser = await User.findOne({ email });
-    if (checkUser)
+    if (!checkUser)
       return res.json({
         success: false,
         message: "Usuário não existe! Crie uma conta",
