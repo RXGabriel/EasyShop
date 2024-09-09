2d5c2f564547f41d8a2bfd982e2395771e033f56
RXGabriel <gabriel2005rf@gmail.com>
2024-09-09 11:22:05 -0300

    feat: add image upload route

---
+++ server/routes/admin/product-routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { upload } = require("../../utils/cloudinary");
+const {
+  handleImageUpload,
+} = require("../../controllers/admin/products-controller");
+
+const router = express.Router();
+
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+
+module.exports = router;
