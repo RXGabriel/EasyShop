e03bc21935e07634c719142f10113c2a16da8b22
RXGabriel <gabriel2005rf@gmail.com>
2024-09-10 15:09:30 -0300

    feat: add product fetching logic to AdminProductsSlice

---
+++ client/src/store/admin-slice/products-slice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+export const fetchAllProducts = createAsyncThunk(
+  "/products/fetchAllProducts",
+  async () => {
+    const result = await axios.get(
+      "http://localhost:5000/api/admin/products/get"
+    );
+
+    return result?.data;
+  }
+);
+
+const AdminProductsSlice = createSlice({
+  name: "adminProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
+});
+
+export default AdminProductsSlice.reducer;
