23e4d6691755eefaeaf055f29e1bad236cd4e824
RXGabriel <gabriel2005rf@gmail.com>
2024-09-10 11:18:46 -0300

    chore: rename fetchAllProducts route

--- server/routes/admin/product-routes.js
+++ server/routes/admin/product-routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
 router.post("/add", addProduct);
-router.get("/get", fetchAllProducts);
+router.get("/products", fetchAllProducts);
 router.put("/edit/:id", editProduct);
 router.delete("/delete/:id", deleteProduct);
 
