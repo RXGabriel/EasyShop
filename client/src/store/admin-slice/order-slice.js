98e759abe73e3f2438aa6b69d25f3e78e7ca7b45
RXGabriel <gabriel2005rf@gmail.com>
2024-09-12 16:03:57 -0300

    feat: add adminOrderSlice

---
+++ client/src/store/admin-slice/order-slice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  orderList: [],
+  orderDetails: null,
+};
+
+export const getAllOrdersForAdmin = createAsyncThunk(
+  "/order/getAllOrdersForAdmin",
+  async () => {
+    const response = await axios.get(
+      `http://localhost:5000/api/admin/orders/get`
+    );
+    return response.data;
+  }
+);
+
+const adminOrderSlice = createSlice({
+  name: "adminOrderSlice",
+  initialState,
+  reducers: {
+    resetOrderDetails: (state) => {
+      console.log("resetOrderDetails");
+      state.orderDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      });
+  },
+});
+
+export const { resetOrderDetails } = adminOrderSlice.actions;
+export default adminOrderSlice.reducer;
