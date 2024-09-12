44aeb67485f68ae9c1ebb8731dd371835d1b701d
RXGabriel <gabriel2005rf@gmail.com>
2024-09-12 15:35:22 -0300

    feat: add getOrderDetailsForAdmin controller

--- server/controllers/admin/order-controller.js
+++ server/controllers/admin/order-controller.js
@@ -49,4 +49,35 @@ const getOrderDetailsForAdmin = async (req, res) => {
   }
 };
 
-module.exports = { getAllOrdersOfUsers, getOrderDetailsForAdmin };
+const updateOrderStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { orderStatus } = req.body;
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Nenhum pedido encontrado",
+      });
+    }
+
+    await Order.findByIdAndUpdate(id, { orderStatus });
+    res.status(200).json({
+      success: true,
+      message: "Status do pedido atualizado com sucesso",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Ocorreu um erro",
+    });
+  }
+};
+
+module.exports = {
+  getAllOrdersOfUsers,
+  getOrderDetailsForAdmin,
+  updateOrderStatus,
+};
