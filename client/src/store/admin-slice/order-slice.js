6660fa064433781dc10f4985ea76e5f3e126af42
RXGabriel <gabriel2005rf@gmail.com>
2024-09-12 16:17:24 -0300

    feat: add getOrderDetailsForAdmin to adminOrderSlice

--- client/src/store/admin-slice/order-slice.js
+++ client/src/store/admin-slice/order-slice.js
@@ -16,6 +16,16 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   }
 );
 
+export const getOrderDetailsForAdmin = createAsyncThunk(
+  "/order/getOrderDetailsForAdmin",
+  async (id) => {
+    const response = await axios.get(
+      `http://localhost:5000/api/admin/orders/details/${id}`
+    );
+    return response.data;
+  }
+);
+
 const adminOrderSlice = createSlice({
   name: "adminOrderSlice",
   initialState,
@@ -37,6 +47,17 @@ const adminOrderSlice = createSlice({
       .addCase(getAllOrdersForAdmin.rejected, (state) => {
         state.isLoading = false;
         state.orderList = [];
+      })
+      .addCase(getOrderDetailsForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderDetailsForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getOrderDetailsForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = null;
       });
   },
 });
