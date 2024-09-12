174bbbac3f16467986c17efd9208b5149b5ccaa9
RXGabriel <gabriel2005rf@gmail.com>
2024-09-12 16:29:35 -0300

    feat: add updateOrderStatus thunk to adminOrderSlice

--- client/src/store/admin-slice/order-slice.js
+++ client/src/store/admin-slice/order-slice.js
@@ -16,6 +16,17 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   }
 );
 
+export const updateOrderStatus = createAsyncThunk(
+  "/order/updateOrderStatus",
+  async ({ id, orderStatus }) => {
+    const response = await axios.put(
+      `http://localhost:5000/api/admin/orders/update/${id}`,
+      { orderStatus }
+    );
+    return response.data;
+  }
+);
+
 export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
