86400fe17130885d54e9cad3093aab7a510feab2
RXGabriel <gabriel2005rf@gmail.com>
2024-09-16 11:23:33 -0300

    feat: add order-details component

---
+++ client/src/components/admin-view/order-details.jsx
@@ -0,0 +1,149 @@
+import { Badge } from "lucide-react";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getOrderDetailsForAdmin,
+  updateOrderStatus,
+} from "@/store/admin-slice/order-slice";
+import { toast } from "@/hooks/use-toast";
+import { DialogContent } from "@radix-ui/react-dialog";
+import CommonForm from "../common/form";
+
+const initialFormData = {
+  status: "",
+};
+
+function AdminOrderDetailsView({ orderDetails }) {
+  const [formData, setFormData] = useState({ initialFormData });
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  function handleUpdateStatus(event) {
+    event.preventDefault();
+    const { status } = formData;
+
+    dispatch(
+      updateOrderStatus({ id: orderDetails?._id, orderStatus: status })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getOrderDetailsForAdmin(orderDetails?._id));
+        dispatch(getOrderDetailsForAdmin());
+        setFormData(initialFormData);
+
+        toast({ title: data?.payload?.message });
+      }
+    });
+  }
+
+  return (
+    <DialogContent className="sm:max-w-[600px]">
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">ID do Pedido</p>
+            <Label>{orderDetails?._id}</Label>
+          </div>
+
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Data do Pedido</p>
+            <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
+          </div>
+
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Preço do Pedido</p>
+            <Label>{orderDetails?.totalAmount}</Label>
+          </div>
+
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Método de Pagamento</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Status do Pagamento</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
+          </div>
+
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Status do Pedido</p>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500"
+                    : orderDetails?.orderStatus === "rejected"
+                    ? "bg-red-600"
+                    : "bg-black"
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
+          </div>
+        </div>
+
+        <Separator />
+
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Detalhes do Pedido</div>
+            <ul className="grid gap-3">
+              {orderDetails?.cardItems && orderDetails?.cardItems.length > 0
+                ? orderDetails?.cardItems.map((item, index) => (
+                    <li
+                      key={index}
+                      className="flex items-center justify-between"
+                    >
+                      <span>Título: {item.title}</span>
+                      <span>Quantidade: {item.quantity}</span>
+                      <span>Preço: {item.price}</span>
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </div>
+        </div>
+
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Informações de Envio</div>
+            <div className="grid gap-0.5 text-muted-foreground">
+              <span>{user.userName}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <CommonForm
+            formControls={[
+              {
+                label: "Status do Pedido",
+                name: "status",
+                componentType: "select",
+                options: [
+                  { id: "pending", label: "Pendente" },
+                  { id: "inProcess", label: "Em Processamento" },
+                  { id: "inShipping", label: "Em Transporte" },
+                  { id: "delivered", label: "Entregue" },
+                  { id: "rejected", label: "Rejeitado" },
+                ],
+              },
+            ]}
+            formData={formData}
+            setFormData={setFormData}
+            buttonText={"Atualizar Status do Pedido"}
+            onSubmit={handleUpdateStatus}
+          />
+        </div>
+      </div>
+    </DialogContent>
+  );
+}
+export default AdminOrderDetailsView;
