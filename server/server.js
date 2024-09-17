f1c528ad025f5c537e218deb4b0a4c9287125651
RXGabriel <gabriel2005rf@gmail.com>
2024-09-17 11:32:25 -0300

    feat: add commonFeatureRouter to server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/product-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
+const commonFeatureRouter = require("./routes/common/feature-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -38,6 +39,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.get("/", (req, res) => {
   res.send("API rodando");
