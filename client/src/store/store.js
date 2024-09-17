e68382cc87afc44e0e099a11bc543d9df3ee74aa
RXGabriel <gabriel2005rf@gmail.com>
2024-09-17 10:59:50 -0300

    feat: add admin order slice to store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin-slice/products-slice";
+import adminOrderSlice from "./admin-slice/order-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
   },
 });
 
