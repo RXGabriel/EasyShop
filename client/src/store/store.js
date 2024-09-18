d794e9672350dc956b24d78d39d70e2792777230
RXGabriel <gabriel2005rf@gmail.com>
2024-09-18 10:31:44 -0300

    feat: add common feature slice to store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin-slice/products-slice";
 import adminOrderSlice from "./admin-slice/order-slice";
+import commonFeatureSlice from "./common-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
     adminOrder: adminOrderSlice,
+    commonFeature: commonFeatureSlice,
   },
 });
 
