8998bf7f25d6ec888fef02918c75e9ad46f40646
RXGabriel <gabriel2005rf@gmail.com>
2024-09-18 11:14:04 -0300

    feat: add feature image upload functionality to admin dashboard page

--- client/src/pages/admin-view/dashboard.jsx
+++ client/src/pages/admin-view/dashboard.jsx
@@ -1,4 +1,59 @@
+import ProductImageUpload from "@/components/admin-view/image-upload";
+import { Button } from "@/components/ui/button";
+import { addFeatureImage, getFeatureImages } from "@/store/common-slice";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function AdminDashboard() {
-    return <div>Admin dashboard</div>
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [imageLoadingState, setImageLoadingState] = useState(false);
+  const dispatch = useDispatch();
+  const { featureImageList } = useSelector((state) => state.commonFeature);
+
+  function handleUploadFeatureImage() {
+    dispatch(addFeatureImage(uploadedImageUrl)).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getFeatureImages());
+        setImageFile(null);
+        setUploadedImageUrl("");
+      }
+    });
+  }
+
+  useState(() => {
+    dispatch(getFeatureImages());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ProductImageUpload
+        imageFile={imageFile}
+        setImageFile={setImageFile}
+        uploadedImageUrl={uploadedImageUrl}
+        setUploadedImageUrl={setUploadedImageUrl}
+        imageLoadingState={imageLoadingState}
+        setImageLoadingState={setImageLoadingState}
+        isCustomStyling={true}
+      />
+
+      <Button onClick={handleUploadFeatureImage} className="mt-5 w-full">
+        Enviar
+      </Button>
+
+      <div className="flex flex-col gap-4 mt-5">
+        {featureImageList && featureImageList.length > 0
+          ? featureImageList.map((featureImgItem, index) => (
+              <div key={index} className="relative">
+                <img
+                  src={featureImgItem.image}
+                  className="w-full h-[300px] object-cover rounded-t-lg"
+                />
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
 }
-export default AdminDashboard
+export default AdminDashboard;
