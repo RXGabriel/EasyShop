720afd0b7cb1f280b3341432bbb4204cd9a7a376
RXGabriel <gabriel2005rf@gmail.com>
2024-09-18 16:45:00 -0300

    feat: add updateCartItemQuantity controller

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -98,4 +98,61 @@ const fetchCartItems = async (req, res) => {
   }
 };
 
-module.exports = { addToCart, fetchCartItems };
+const updateCartItemQuantity = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Credenciais inválidas",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Carrinho não encontrado",
+      });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (findCurrentProductIndex === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Item do carrinho não encontrado",
+      });
+    }
+
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Produto não encontrado",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: { ...cart._doc, items: populateCartItems },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Ocorreu um erro",
+    });
+  }
+};
+
+module.exports = { addToCart, fetchCartItems, updateCartItemQuantity };
