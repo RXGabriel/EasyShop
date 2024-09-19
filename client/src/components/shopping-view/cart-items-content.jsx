4bb90553b6efdb901a5e934ad7df435386c6f247
RXGabriel <gabriel2005rf@gmail.com>
2024-09-19 11:45:56 -0300

    feat: add handleUpdateQuantity function to UserCartItemsContent component

---
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -0,0 +1,96 @@
+import { toast } from "@/hooks/use-toast";
+import { updateCartQuantity } from "@/store/shop-slice/cart-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "../ui/button";
+import { Minus, Plus } from "lucide-react";
+
+function UserCartItemsContent({ cartItem }) {
+  const { cartItems } = useSelector((state) => state.shopCart);
+  const { productList } = useSelector((state) => state.shopProducts);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  function handleUpdateQuantity(getCartItem, typeOfAction) {
+    if (typeOfAction == "plus") {
+      let getCartItems = cartItems.items || [];
+
+      if (getCartItems.length) {
+        const indexOfCurrentCartItem = getCartItem.findIndex(
+          (item) => item.productId === getCartItem?.productId
+        );
+
+        const getCurrentProductIndex = productList.findIndex(
+          (product) => product._id === getCartItem?.productId
+        );
+
+        const getTotalStock = productList[getCurrentProductIndex].totalStock;
+
+        if (indexOfCurrentCartItem > -1) {
+          const getQuantity = getCartItems[indexOfCurrentCartItem].quantity;
+          if (getQuantity + 1 > getTotalStock) {
+            toast({
+              title: `Apenas ${getQuantity} quantidade pode ser adicionada ao item`,
+              variant: "destructive",
+            });
+            return;
+          }
+        }
+      }
+    }
+
+    dispatch(
+      updateCartQuantity({
+        userId: user?.id,
+        productId: getCartItem?.productId,
+        quantity:
+          typeOfAction === "plus"
+            ? getCartItem?.quantity + 1
+            : getCartItem?.quantity - 1,
+      })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: "Item do carrinho atualizado com sucesso",
+        });
+      }
+    });
+  }
+
+  return (
+    <div className="flex items-center space-x-4">
+      <img
+        src={cartItem?.image}
+        alt={cartItem?.title}
+        className="w-20 h-20 rounded object-cover"
+      />
+      <div className="flex-1">
+        <h3 className="font-extrabold">{cartItem?.title}</h3>
+        <div className="flex items-center gap-2 mt-1">
+          <Button
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
+            disabled={cartItem?.quantity === 1}
+            onClick={() => handleUpdateQuantity(cartItem, "minus")}
+          >
+            <Minus className="w-4 h-4" />
+            <span className="sr-only">Diminuir</span>
+          </Button>
+
+          <span className="font-semibold">{cartItem?.quantity}</span>
+          <Button
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
+            onClick={() => handleUpdateQuantity(cartItem, "plus")}
+          >
+            <Plus className="w-4 h-4" />
+            <span className="sr-only">Aumentar</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserCartItemsContent;
