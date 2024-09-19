070d4f8d126a31e749f7cd197777e0ddb939eb0b
RXGabriel <gabriel2005rf@gmail.com>
2024-09-19 10:00:35 -0300

    feat: add fetchCartItems async thunk

--- client/src/store/shop-slice/cart-slice.js
+++ client/src/store/shop-slice/cart-slice.js
@@ -17,6 +17,16 @@ export const addToCart = createAsyncThunk(
   }
 );
 
+export const fetchCartItems = createAsyncThunk(
+  "cart/fetchCartItems",
+  async (userId) => {
+    const response = await axios.get(
+      `http://localhost:5000/api/shop/cart/get/${userId}`
+    );
+    return response.data;
+  }
+);
+
 const shoppingCartSlice = createSlice({
   name: "shoppingCart",
   initialState,
@@ -26,11 +36,22 @@ const shoppingCartSlice = createSlice({
       .addCase(addToCart.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(addToCart.pending, (state, action) => {
+      .addCase(addToCart.fulfilled, (state, action) => {
         state.isLoading = false;
         state.cartItems = action.payload.data;
       })
-      .addCase(addToCart.pending, (state) => {
+      .addCase(addToCart.rejected, (state) => {
+        state.isLoading = false;
+        state.cartItems = [];
+      })
+      .addCase(fetchCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchCartItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload.data;
+      })
+      .addCase(fetchCartItems.rejected, (state) => {
         state.isLoading = false;
         state.cartItems = [];
       });
