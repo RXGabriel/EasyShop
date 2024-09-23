90a0d8d97dd0bae4ae706d1ae2f82d3fc1215935
RXGabriel <gabriel2005rf@gmail.com>
2024-09-23 16:14:01 -0300

    feat: add product-tile component

---
+++ client/src/components/shopping-view/product-tile.jsx
@@ -0,0 +1,83 @@
+import { brandOptionsMap, categoryOptionsMap } from "@/config";
+import { Badge } from "../ui/badge";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Button } from "../ui/button";
+
+function ShoppingProductTile({
+  handleGetProductDetails,
+  product,
+  handleAddToCart,
+}) {
+  return (
+    <Card className="w-full max-w-sm mx-auto">
+      <div onClick={() => handleGetProductDetails(product?._id)}>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-full h-[300px] object-cover rounded-t-lg"
+          />
+
+          {product?.totalStock === 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Sem Estoque
+            </Badge>
+          ) : product?.totalStock < 10 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              {`Somente ${product?.totalStock} items restantes`}
+            </Badge>
+          ) : product?.salePrice > 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Venda
+            </Badge>
+          ) : null}
+        </div>
+
+        <CardContent className="p-4">
+          <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-[16px] text-muted-foreground">
+              {categoryOptionsMap[product?.category]}
+            </span>
+            <span className="text-[16px] text-muted-foreground">
+              {brandOptionsMap[product?.brand]}
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center mb-2">
+            <span
+              className={`${
+                product?.salePrice > 0 ? "line-through" : ""
+              } text-lg font-semibold text-primary`}
+            >
+              R${product?.price}
+            </span>
+
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-semibold text-primary">
+                R${product?.salePrice}
+              </span>
+            ) : null}
+          </div>
+        </CardContent>
+      </div>
+
+      <CardFooter>
+        {product?.totalStock === 0 ? (
+          <Button className="w-full opacity-60 cursor-not-allowed">
+            Sem Estoque
+          </Button>
+        ) : (
+          <Button
+            onClick={() => handleAddToCart(product?._id, product?.totalStock)}
+            className="w-full"
+          >
+            Adicionar no carrinho
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ShoppingProductTile;
