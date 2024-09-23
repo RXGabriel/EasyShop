0b3d5d075441f4cb44c9bd2e733896eecc2cdc03
RXGabriel <gabriel2005rf@gmail.com>
2024-09-23 15:21:17 -0300

    feat: add fetchAllFilteredProducts async thunk

---
+++ client/src/store/products-slice/index.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+  productDetails: null,
+};
+
+export const fetchAllFilteredProducts = createAsyncThunk(
+  "/products/fetchAllFilteredProducts",
+  async ({ filteredParams, sortParams }) => {
+    const query = new URLSearchParams({
+      ...filteredParams,
+      sortBy: sortParams,
+    });
+
+    const result = await axios.get(
+      `http://localhost:5000/api/shop/products/get?${query}`
+    );
+
+    return result?.data;
+  }
+);
+
+const shoppingProductSlice = createSlice({
+  name: "shoppingProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
+});
+
+export default shoppingProductSlice.reducer;
