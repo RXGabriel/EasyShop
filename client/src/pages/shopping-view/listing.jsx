5127b25fec78557a0dd638149434783c9ea09a3a
RXGabriel <gabriel2005rf@gmail.com>
2024-09-25 10:00:42 -0300

    feat: add product filtering and sorting functionality

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,4 +1,86 @@
+import ProductFilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { ArrowDownIcon } from "lucide-react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function ShoppingListing() {
-  return <div>listing</div>;
+  const [filters, setFilters] = useState({});
+  const { productList, productDetails } = useSelector(
+    (state) => state.shopProducts
+  );
+  const [sort, setSort] = useState(null);
+
+  function handleFilter(getSectionId, getCurrentOption) {
+    let cpyFilters = { ...filters };
+    const indexOfCurrentSection = Object.keys(cpyFilters).indexOf(getSectionId);
+
+    if (indexOfCurrentSection === -1) {
+      cpyFilters = { ...cpyFilters, [getSectionId]: [getCurrentOption] };
+    } else {
+      const indexOfCurrentOption =
+        cpyFilters[getSectionId].indexOf(getCurrentOption);
+
+      if (indexOfCurrentOption === -1)
+        cpyFilters[getSectionId].push(getCurrentOption);
+      else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1);
+    }
+
+    setFilters(cpyFilters);
+    sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+  }
+
+  function handleSort(value) {
+    setSort(value);
+  }
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter filters={filters} handleFilter={handleFilter} />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">Todos os produtos</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">
+              {productList?.length} Produtos
+            </span>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowDownIcon className="h-4 w-4" />
+                  <span>Ordenar por</span>
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ShoppingListing;
