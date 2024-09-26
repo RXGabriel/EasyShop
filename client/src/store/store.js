990428792488fac4b3cc3d80a13da8b89855bcb1
RXGabriel <gabriel2005rf@gmail.com>
2024-09-26 11:39:40 -0300

    chore: move products-slice and review-slice to shop directory

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,8 +3,8 @@ import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin-slice/products-slice";
 import adminOrderSlice from "./admin-slice/order-slice";
 import commonFeatureSlice from "./common-slice";
-import shopProductsSlice from "./products-slice";
-import shopReviewSlice from "./review-slice";
+import shopProductsSlice from "./shop-slice/products-slice";
+import shopReviewSlice from "./shop-slice/review-slice";
 
 const store = configureStore({
   reducer: {
