aabb2c571d4f939dde7cdda79ddf6f1a78f1578a
RXGabriel <gabriel2005rf@gmail.com>
2024-09-30 10:03:00 -0300

    fix: add optional chaining to product description

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -109,7 +109,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
         <div>
           <h1 className="text-3xl font-extrabold">{productDetails?.title} </h1>
           <p className="text-muted-foreground text-2xl mb-5 mt-4">
-            {productDetails.description}
+            {productDetails?.description}
           </p>
         </div>
 
