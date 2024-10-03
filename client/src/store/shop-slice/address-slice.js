d7a4a502c152a2f27a3b3d3adea6f3d412a7ba9b
RXGabriel <gabriel2005rf@gmail.com>
2024-10-03 09:56:26 -0300

    feat: implement addNewAddress async thunk

---
+++ client/src/store/shop-slice/address-slice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  addressList: [],
+};
+
+export const addNewAddress = createAsyncThunk(
+  "/address/addNewAddress",
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/shop/address/add",
+      formData
+    );
+    return response.data;
+  }
+);
+
+const addressSlice = createSlice({
+  name: "address",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addNewAddress.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addNewAddress.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(addNewAddress.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export default addressSlice.reducer;
