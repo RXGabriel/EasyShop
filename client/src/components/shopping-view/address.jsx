93c27ed9f5c3177a45c815c655521cd52eebe8e9
RXGabriel <gabriel2005rf@gmail.com>
2024-10-03 15:18:18 -0300

    feat: add edit and delete functionality to address component

---
+++ client/src/components/shopping-view/address.jsx
@@ -0,0 +1,70 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Card } from "../ui/card";
+import AddressCard from "./address-card";
+import { useState } from "react";
+import {
+  deleteAddress,
+  fetchAllAddresses,
+} from "@/store/shop-slice/address-slice";
+import { useToast } from "@/hooks/use-toast";
+
+const initialAddressFormData = {
+  address: "",
+  city: "",
+  phone: "",
+  pincode: "",
+  notes: "",
+};
+
+function Address({ selectedId, setCurrentSelectedAddress }) {
+  const { addressList } = useSelector((state) => state.shopAddress);
+  const [currentEditedId, setCurrentEditedId] = useState(null);
+  const [formData, setFormData] = useState(initialAddressFormData);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { toast } = useToast();
+
+  function handleEditAddress(getCurrentAddress) {
+    setCurrentEditedId(getCurrentAddress?._id);
+    setFormData({
+      ...formData,
+      address: getCurrentAddress?.address,
+      city: getCurrentAddress?.city,
+      phone: getCurrentAddress?.phone,
+      pincode: getCurrentAddress?.pincode,
+      notes: getCurrentAddress?.notes,
+    });
+  }
+
+  function handleDeleteAddress(getCurrentAddress) {
+    dispatch(
+      deleteAddress({ userId: user?.id, addressId: getCurrentAddress._id })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchAllAddresses(user?.id));
+        toast({ title: "Endereço deletado com sucesso" });
+      }
+    });
+  }
+
+  return (
+    <Card>
+      <div className="mb-5 p-3 grid grid-cols-1 sm:grid-cols-2 gap-2">
+        {addressList && addressList.length > 0
+          ? addressList.map((singleAddressItem) => (
+              <AddressCard
+                key={singleAddressItem._id}
+                selectedId={selectedId}
+                handleDeleteAddress={handleDeleteAddress}
+                addressInfo={singleAddressItem}
+                handleEditAddress={handleEditAddress}
+                setCurrentSelectedAddress={setCurrentSelectedAddress}
+              />
+            ))
+          : null}
+      </div>
+    </Card>
+  );
+}
+
+export default Address;
