51a21c2601619820cba9d46459b7565cd6b05576
RXGabriel <gabriel2005rf@gmail.com>
2024-10-04 10:38:41 -0300

    feat: add Badge component for order details component

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
+import { Badge } from "../ui/badge";
 import { DialogContent } from "../ui/dialog";
 import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
 
 function ShoppingOrderDetailsView({ orderDetails }) {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <DialogContent className="sm:max-w-[600px]">
       <div className="grid gap-6">
@@ -31,7 +36,58 @@ function ShoppingOrderDetailsView({ orderDetails }) {
             <Label>{orderDetails?.paymentStatus}</Label>
           </div>
 
-          <div></div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Status do pedido</p>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500"
+                    : orderDetails?.orderStatus === "rejected"
+                    ? "bg-red-600"
+                    : "bg-black"
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
+          </div>
+        </div>
+
+        <Separator />
+
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Detalhes do pedido</div>
+            <ul className="grip gap-3">
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li
+                      key={item.id}
+                      className="flex items-center justify-between"
+                    >
+                      <span>Titulo: {item.title}</span>
+                      <span>Quantidade: {item.quantity}</span>
+                      <span>Preço: ${item.price}</span>
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </div>
+        </div>
+
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Detalhes do compra</div>
+            <div className="grid gap-0.5 text-muted-foreground">
+              <span>{user.userName}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
+            </div>
+          </div>
         </div>
       </div>
     </DialogContent>
