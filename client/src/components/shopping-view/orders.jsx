0ee54cefef511c19f84a7b5b5124df3352e1bf00
RXGabriel <gabriel2005rf@gmail.com>
2024-10-04 11:07:08 -0300

    feat: add display and fetch order details

---
+++ client/src/components/shopping-view/orders.jsx
@@ -0,0 +1,97 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Badge } from "../ui/badge";
+import { Dialog } from "../ui/dialog";
+import { useState } from "react";
+import { resetOrderDetails } from "@/store/admin-slice/order-slice";
+import { Button } from "../ui/button";
+import { getOrderDetails } from "@/store/shop-slice/order-slice";
+import ShoppingOrderDetailsView from "./order-details";
+
+function ShoppingOrders() {
+  const { orderList, orderDetails } = useSelector((state) => state.shopOrder);
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const dispatch = useDispatch();
+
+  function handleFetchOrderDetails(getId) {
+    dispatch(getOrderDetails(getId));
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Histórico de pedidos</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID do pedido</TableHead>
+              <TableHead>Data do pedido</TableHead>
+              <TableHead>Status do pedido</TableHead>
+              <TableHead>Preço do pedido</TableHead>
+              <TableHead>
+                <span className="sr-only">Detalhes</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
+                  <TableRow key={orderItem?._id}>
+                    <TableCell>{orderItem?._id}</TableCell>
+                    <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
+                    <TableCell>
+                      <Badge
+                        className={`py-1 px-3 ${
+                          orderItem?.orderStatus === "confirmed"
+                            ? "bg-green-500"
+                            : orderItem?.orderStatus === "rejected"
+                            ? "bg-red-600"
+                            : "bg-black"
+                        }`}
+                      >
+                        {orderItem?.orderStatus}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>R${orderItem?.totalAmount}</TableCell>
+                    <TableCell>
+                      <Dialog
+                        open={openDetailsDialog}
+                        onOpenChange={() => {
+                          setOpenDetailsDialog(false);
+                          dispatch(resetOrderDetails());
+                        }}
+                      >
+                        <Button
+                          onClick={() =>
+                            handleFetchOrderDetails(orderItem?._id)
+                          }
+                        >
+                          Ver detalhes
+                        </Button>
+
+                        <ShoppingOrderDetailsView orderDetails={orderDetails} />
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ShoppingOrders;
