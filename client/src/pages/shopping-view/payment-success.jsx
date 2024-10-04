f56b9e08efa680ec83da596e97651b63763e0839
RXGabriel <gabriel2005rf@gmail.com>
2024-10-04 14:59:14 -0300

    feat: add PaymentSuccessPage

---
+++ client/src/pages/shopping-view/payment-success.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNavigate } from "react-router-dom";
+
+function PaymentSuccessPage() {
+  const navigate = useNavigate();
+
+  return (
+    <Card>
+      <CardHeader className="p-0">
+        <CardTitle className="text-4xl">
+          O pagamento foi realizado com sucesso!
+        </CardTitle>
+      </CardHeader>
+
+      <Button className="mt-5" onClick={() => navigate("/shop/account")}>
+        Ver pedidos
+      </Button>
+    </Card>
+  );
+}
+
+export default PaymentSuccessPage;
